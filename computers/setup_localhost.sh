f9230fdfc93f47974262d9393c70dbf60e45eb49
superstar54 <xingwang1991@gmail.com>
2024-01-21 12:52:22 +0100

    udpate eiger

--- computers/setup_localhost.sh
+++ computers/setup_localhost.sh
@@ -1,1 +1,1 @@
-verdi computer setup -L localhost -H localhost -T core.local -S core.direct -w `~/data/aiida_work/aiida` 
+verdi computer setup -L localhost -H localhost -T core.local -S core.direct -w ~/datas/aiida_work/science
